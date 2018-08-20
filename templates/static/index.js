05ca647bd7d06642ab816d78836ed27cdc1cafe5
wahiddyno <wahiddyno@users.noreply.github.com>
2018-08-20 15:13:46 +0530

    minor changes

--- templates/static/index.js
+++
@@ -1,6 +0,0 @@
-console.log("linked");
-var doc = new jsPDF();
-$('#btn').click(function () {
-    doc.fromHTML($('body').html(), 15, 15);
-    doc.save('sample-file.pdf');
-});
--- test.py
+++ test.py
@@ -1,2 +1,18 @@
+from flask import Flask,render_template,make_response
 import pdfkit
-pdfkit.from_file('index.html', 'out.pdf')
+
+app = Flask('__name__')
+
+@app.route('/generate')#http://127.0.0.1:5000/generate
+def generate():
+    render = render_template('index.html',name='wahid')
+    pdf = pdfkit.from_string(render, False)
+
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application.pdf'
+    response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
